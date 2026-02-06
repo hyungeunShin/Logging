f74074a375438d5434859d9608bf4769fa9cb169
hyungeunShin <123938245+hyungeunShin@users.noreply.github.com>
2026-02-06 17:53:53 +0900

    logging

--- src/main/java/com/example/logging/domain/ShortenUrl.java
+++ src/main/java/com/example/logging/domain/ShortenUrl.java
@@ -2,11 +2,13 @@ package com.example.logging.domain;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.Random;
 
 @Getter
 @Setter
+@ToString
 public class ShortenUrl {
     private String originalUrl;
     private String shortenUrlKey;
